1390ad9679f34a8d0ad80fc81819a9bcc947531f
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2021-03-19 05:47:32 -0500

    agregando firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDA44YsJU4mzpoaKO-kjSN5WZNT63STRm8",
+    authDomain: "comfeco-ed3d9.firebaseapp.com",
+    projectId: "comfeco-ed3d9",
+    storageBucket: "comfeco-ed3d9.appspot.com",
+    messagingSenderId: "544755262368",
+    appId: "1:544755262368:web:07b71a839276c488dcfac5",
+    measurementId: "G-XG15JE912L"
+  }
 };
